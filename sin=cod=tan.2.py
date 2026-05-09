718427ec4d019377c533673c32d6836ae91abbfc
mmgymgy <mingi091110@gmail.com>
2026-05-09 14:53:01 +0900

    Update and rename sin=cod=tan.1.py to sin=cod=tan.2.py

---
+++ sin=cod=tan.2.py
@@ -0,0 +1,78 @@
+import matplotlib.pyplot as plt
+import math
+
+
+def sin(a,b,c,d,q1,q2):
+    x = []
+    y = []
+   
+    for i in range(-360 + (q1 * 360),361 + (q2 * 360)):
+        rad = math.radians(i)
+        x.append(rad/b-c)
+        y.append((math.sin(rad))*a+d)
+   
+    plt.plot(x,y)
+    plt.axhline(0, color='red', linewidth=1)
+    plt.axvline(0, color='red', linewidth=1)
+    plt.grid()
+    plt.show()
+
+
+def cos(a,b,c,d,q1,q2):
+    x = []
+    y = []
+   
+    for i in range(-360 + (q1 * 360),361 + (q2 * 360)):
+        rad = math.radians(i)
+        x.append(rad/b-c)
+        y.append((math.cos(rad))*a+d)
+   
+    plt.plot(x,y)
+    plt.axhline(0, color='red', linewidth=1)
+    plt.axvline(0, color='red', linewidth=1)
+    plt.grid()
+    plt.show()
+
+   
+"""def tan(a,b,c,d):
+    x = []
+    y = []
+   
+    for i in range(0,361):
+        rad = math.radians(i)
+        x.append(rad/b-c)
+        y.append((math.tan(rad))*a+d)
+   
+    plt.plot(x,y)
+    plt.axhline(0, color='red', linewidth=1)
+    plt.axvline(0, color='red', linewidth=1)
+    plt.grid()
+    plt.show()"""
+
+
+
+q = input("sin,cos,tan중 어떤 그래프: ")
+
+a = int(input(f"a{q}(bx + c) + d 의 a값: "))
+b = int(input(f"a{q}(bx + c) + d 의 b값: "))
+c = int(input(f"a{q}(bx + c) + d 의 c값: "))
+d = int(input(f"a{q}(bx + c) + d 의 d값: "))
+
+if c>0:
+    q1 = 0
+    q2 = c//4
+    print(f"1번{q2}")
+elif c<0:
+    q1 = c//4
+    q2 = 0
+    print(f"2번{q1}")
+else:
+    q1 = q2 = 0
+    print("3번")
+
+if q == "sin":
+    sin(a,b,c,d,q1,q2)
+elif q == "cos":
+    cos(a,b,c,d,q1,q2)
+elif q == "tan":
+    tan(a,b,c,d)
